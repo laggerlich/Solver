f58dc535475d1f5d5eee429b0df8cbde633c792e
laggerlich <bonjour937@icloud.com>
2021-07-12 13:45:43 +0300

    First try

---
+++ src/First/Solver.java
@@ -0,0 +1,35 @@
+package First;
+
+import java.util.Scanner;
+
+public class Solver {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double c = in.nextDouble();
+
+        Discriminant d = new Discriminant(a, b, c);
+
+        double x1 = (-b+Math.pow(d.getDiscriminant(), 0.5))/(2*a);
+        double x2 = (-b-Math.pow(d.getDiscriminant(), 0.5))/(2*a);
+        System.out.println(x1 + " " + x2);
+    }
+
+    static class Discriminant {
+        private double a;
+        private double b;
+        private double c;
+
+        public Discriminant(double a, double b, double c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double getDiscriminant() {
+            return b * b - 4 * a * c;
+        }
+    }
+}
